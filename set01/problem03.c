e539b9e66cac7e799bcc9f04e13f275ad7d9c19a
Shinoria Rehman <114480292+shinoriaa@users.noreply.github.com>
2022-10-13 10:40:56 +0000

    prob 3 solved

---
+++ set01/problem03.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+{
+  int main ()
+  int num1, num2, result;
+  printf("\nEnter two number: ");
+  scanf("%d %d", &num1, &num2);
+  result=add(num1, num2);
+  printf("\nAddition of %d and %d is %d", num1, num2, result);
+  return1 ;
+}
