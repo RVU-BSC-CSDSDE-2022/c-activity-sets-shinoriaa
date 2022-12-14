4e93b6573b4b1e1a7acf74189204b02b94b31750
Shinoria Rehman <114480292+shinoriaa@users.noreply.github.com>
2022-12-14 06:54:51 +0000

    DONE

--- set01/problem02.c
+++ set01/problem02.c
@@ -10,3 +10,13 @@ int main()
     printf("Sum of %d and %d is: %d", num1, num2, sum);
     return 0;
 }
+#include <stdio.h>
+int main()
+{
+  int num1,num2,sum;
+  printf("enter two integers:");
+  scanf("%d %d", &NUM1 &NUM2);
+  sum= nym1 = num2
+    printf("sum of %d amnd %d is %d:")
+}
+int num1 
