a184315da85d5bc0a0611f19ed27485cad8a81c1
Shinoria Rehman <114480292+shinoriaa@users.noreply.github.com>
2022-12-19 07:04:23 +0000

    g

--- set01/problem03.c
+++ set01/problem03.c
@@ -5,6 +5,6 @@
   printf("\nEnter two number: ");
   scanf("%d %d", &num1, &num2);
   result=(num1 +num2);
-  printf("\nAddition of %d and %d is %d", num1, num2, result);
+  printf("\nsum of %d and %d is %d", num1, num2, result);
   return0;
 }
