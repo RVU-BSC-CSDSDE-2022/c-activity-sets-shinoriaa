28b52d07a07f1921ea2b968ac976e927e7f98681
Shinoria Rehman <114480292+shinoriaa@users.noreply.github.com>
2022-12-21 04:28:26 +0000

    done

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-int main(){
-  int d,b,c;
-  d=input;
-  b=input;
-  c=add(d,b);
-    output();
+int main()
+{
+  a=input();
+b=input();
+c=sum(a,b);
+  output();
+  
 }
+int input(int a ,int b)
+{int n;
+  printf("enter your number \n");
+
+  scanf("%d",&n);
+  return n;
+  }
+int sum(int a,int b,int c)
+{*c=*a+*b;
+  return c;
+  }
+void output(int a,int b,int c)
+{printf("sum of %d and %d is %d");
+  }
