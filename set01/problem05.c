4b3ef70b4326cf71940a7dfe634c958d76a3c71e
Shinoria Rehman <114480292+shinoriaa@users.noreply.github.com>
2023-01-14 19:09:12 +0000

    done

--- set01/problem05.c
+++ set01/problem05.c
@@ -3,28 +3,34 @@ int input();
 int compare(int a, int b, int c);
 void output(int a, int b, int c, int largest);
 int main()
-{int a,b,c,largest;
+{
+  int a,b,c,largest;
   a=input();
   b=input();
   c=input();
   largest=compare(a,b,c);
   output(a,b,c,largest);
-  }
+  return 0;
+}
 int input()
-{int n;
-  printf("enter the number");
+{
+  int n;
+  printf("enter an integer of your choice");
   scanf("%d",&n);
-  return n;}
-int compare(int a ,int b,int c)
-{if((a>=b)&(a>=c))
-{return a;}
-else if ((b>=a)&&(b>=c))
-{return b;
-  }
+  return n;
+}
+int compare(int a, int b, int c)
+{
+  int largest;
+  if((a>=b)&&(a>=c))
+  {return a;}
+  else if((b>=a)&&(b>=c))
+  {return b;}
   else if((c>=a)&&(c>=b))
   {return c;}
+  return largest;
   }
-void output(int a,int b,int c,largest)
-{printf("largest number among %d %d %D is %d",a,b,c,largest);}
-
-
+void output(int a,int b,int c,int largest)
+{
+  printf("largest number among %d,%d and %d is %d",a,b,c,largest);
+}
