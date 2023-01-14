ee4cbda365b37cd53302c2e08309aec9df943aff
Shinoria Rehman <114480292+shinoriaa@users.noreply.github.com>
2023-01-14 17:36:40 +0000

    done

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
-{int a,b,sum;
-printf("enter two integers:");
-scanf("%d %d",&a,&b);
-sum=a+b;
-printf("sum of %d and %d is %d",a,b,sum);
-return  0;}
-
+{
+  int a,b,sum;
+  printf("enter two numbers");
+  scanf("%d %d",&a,&b);
+  sum=a+b;
+  printf("sum of %d and %d is %d",a,b,sum);
+  return 0;
+}
